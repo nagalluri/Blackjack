41591afcd6ad22e14872f8b5e2f7504378fb3bf0
Nag Alluri <nagalluri@berkeley.edu>
2018-01-18 10:27:32 -0500

    Implemented Deck functionality

--- Deck.java
+++ Deck.java
@@ -1,6 +1,8 @@
-package blackjack;
+package Blackjack;
 
-import blackjack.Card;
+import Blackjack.Card;
+import java.util.ArrayList;
+import java.util.Collections;
 
 /** 
 * A class for a standard set of 52 playing cards
@@ -8,30 +10,57 @@ import blackjack.Card;
 */
 public class Deck {
 	
+	private ArrayList<Card> cards;	
 
 	/** 
 	* A constructor for the Deck class
 	*/
 	public Deck() {
+		ArrayList<Card> cards = new ArrayList<Card>();
+		String[] values = new String[13];
+		String[] suits = {"Hearts", "Spades", "Clubs", "Diamonds"};
 
+		for (String suit : suits) {
+			for (int i = 1; i <= 13; i++) {
+				String value = Integer.toString(i);
+				if (i == 11) {
+					value = "Jack";
+				} else if (i == 12) {
+					value = "Queen";
+				} else if (i == 13) {
+					value = "King";
+				} else if (i == 1) {
+					value = "Ace";
+				}
+				Card c = new Card(suit, value);
+				cards.add(c);
+			}
+		}
+		this.cards = cards;
 	}
 
 
 	/**
-	* A method that shuffles a deck
-	* @param - d is the deck that is to be shuffled
+	* A method that shuffles and returns a deck
 	*/
-	public static Deck shuffle(Deck d) {
-		return d;
+	public Deck shuffle() {
+		Collections.shuffle(this.cards);
+		return this;
 	}
 
 
 	/**
-	* A method that draws the top card of the deck
-	* @param - d is the deck that the top card is being drawn from
+	* A method that removes and returns the top card of the deck
 	*/
-	public static void draw(Deck d) {
-		return;
+	public Card draw() {
+		return this.cards.remove(0);
+	}
+
+	/**
+	* A method that returns the size of the deck
+	*/
+	public int size() {
+		return this.cards.size();
 	}
 
 
@@ -39,6 +68,12 @@ public class Deck {
 	* Main method
 	*/
 	public static void main(String[] args) {
+		Deck d = new Deck();
+		d.shuffle();
+		System.out.println(d.size());
+		Card c = d.draw();
+		c.print();
+		System.out.println(d.size());
 
 	}
 }
